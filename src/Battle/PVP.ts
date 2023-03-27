0a312c1505ca385c7bd378f541834c654f048639
Lucca Zaghi <lucca.zaghi@gmail.com>
2023-03-27 16:30:36 -0300

    refactor: apaga getters nao usados na classe PVP

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -10,14 +10,6 @@ export default class PVP extends Battle {
     super(player);
   }
 
-  get player1(): Fighter {
-    return this.player;
-  }
-
-  get player2(): Fighter {
-    return this._player2;
-  }
-
   fight() {
     while (this.player.lifePoints > -1 && this._player2.lifePoints > -1) {
       this.player.attack(this._player2);
