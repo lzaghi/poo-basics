0c82a178dec266ff23d84c8cf36f32a43431076a
Lucca Zaghi <lucca.zaghi@gmail.com>
2023-03-27 13:14:05 -0300

    req 7: implementa classe Character

--- src/Character.ts
+++ src/Character.ts
@@ -36,6 +36,10 @@ export default class Character implements Fighter {
     return this._archetype;
   }
 
+  get maxlifePoints(): number {
+    return this._maxLifePoints;
+  }
+
   get lifePoints(): number {
     return this._lifePoints;
   }
@@ -48,27 +52,39 @@ export default class Character implements Fighter {
     return this._defense;
   }
 
+  get dexterity(): number {
+    return this._dexterity;
+  }
+
   get energy(): Energy {
     return { ...this._energy };
   }
 
   receiveDamage(attackPoints: number): number {
-    const damage = this._defense - attackPoints;
+    const damage = attackPoints - this._defense;
     if (damage > 0) this._lifePoints -= damage;
     if (damage <= 0) this._lifePoints -= 1;
     if (this._lifePoints <= 0) return -1;
     return this._lifePoints;
   }
   
-  // attack(enemy: Fighter): void {
-  //   throw new Error('Method not implemented.');
-  // }
+  attack(enemy: Fighter): void {
+    enemy.receiveDamage(this._strength);
+  }
 
   // special?(enemy: Fighter): void {
   //   throw new Error('Method not implemented.');
   // }
 
-  // levelUp(): void {
-  //   throw new Error('Method not implemented.');
-  // }
+  levelUp(): void {
+    this._maxLifePoints += getRandomInt(1, 10);
+    if (this._maxLifePoints >= this._race.maxLifePoints) {
+      this._maxLifePoints = this._race.maxLifePoints;
+    }
+    this._strength += getRandomInt(1, 10);
+    this._dexterity += getRandomInt(1, 10);
+    this._defense += getRandomInt(1, 10);
+    this._energy.amount = 10;
+    this._lifePoints = this._maxLifePoints;
+  }
 }
